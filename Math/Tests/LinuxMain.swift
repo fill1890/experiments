1423dc36dcdebb8e607eb9562d09708fd9d4e3ca
Andrew <andrew.walls2@gmail.com>
2016-05-28 20:49:40 +1000

    Added test suite
    * Added LinuxMain.swift to satisfy Swift PM on Linux
    * Added Arguments.swift to test Arguments module
    * Began implementing test suite for Arguments

---
+++ Math/Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import MathTestSuite
+
+XCTMain([
+    testCase(ArgumentsTests.allTests),
+])
---
+++ Math/Tests/Math/Arguments.swift
@@ -0,0 +1,111 @@
+// TODO: Throw XCTest errors in invalids instead of printing errors
+
+import XCTest
+@testable import Math
+
+class ArgumentsTests: XCTestCase {
+
+    let standardTestContents: [String] = [
+        "",
+        "--help",
+        "add",
+        "1",
+        "2",
+        "-v"
+    ]
+
+    let operationStubs: [String] = ["add", "sub"]
+
+    func testSwitchParse() { 
+        let parseResult = try! parseArguments(standardTestContents, validOps: operationStubs)
+    
+        XCTAssertEqual(
+            parseResult.switches,
+            ["verbose": true, "help": true],
+            "Switches not parsed correctly"
+        )
+    }
+    func testOpParse() {
+        let parseResult = try! parseArguments(standardTestContents, validOps: operationStubs)
+        
+        XCTAssertEqual(
+            parseResult.operation, 
+            "add", 
+            "Operation 'add' not parsed correctly"
+        )
+    }
+    func testArgParse() {
+        let parseResult = try! parseArguments(standardTestContents, validOps: operationStubs)
+        
+        XCTAssertEqual(
+            parseResult.arg1, 
+            1, 
+            "Argument 1 not parsed correctly"
+        )
+        XCTAssertEqual(
+            parseResult.arg2, 
+            2, 
+            "Argument 2 not parsed correctly"
+         )
+    }
+    func testInvalidArg() {
+        do {
+            try parseArguments(["", "add", "one", "two"], validOps: operationStubs)
+        print("ERROR: Invalid argument error not thrown")
+        } catch let error as ArgumentError {
+            XCTAssertEqual(
+                error.description,
+                ArgumentError.InvalidValue.description,
+                "Invalid value error not thrown"
+            )
+        } catch {
+            // Somehow, the thrown error isn't an ArgumentError. HELP
+            print("OH NO")
+        }
+    }
+    func testInvalidOp() {
+        do {
+            try parseArguments(["", "ohno", "1", "2"], validOps: operationStubs)
+            print("ERROR: Invalid operation error not thrown")
+        } catch let error as ArgumentError {
+            XCTAssertEqual(
+                error.description,
+                ArgumentError.InvalidOperation.description,
+                "Invalid value error not thrown"
+            )
+        } catch {
+            // Somehow, the thrown error isn't an ArgumentError. HELP
+            print("OH NO")
+        }
+    }
+    func testInvalidSwitch() {
+        do {
+            try parseArguments(["", "add", "1", "2", "--ohno"], validOps: operationStubs, validSwitches: ["verbose", "help"])
+            print("ERROR: Invalid switch error not thrown")
+        } catch let error as ArgumentError {
+            XCTAssertEqual(
+                error.description,
+                ArgumentError.InvalidSwitch.description,
+                "Invalid switch error not thrown"
+            )
+        } catch {
+            // Somehow, the thrown error isn't an ArgumentError. HELP
+            print("OH NO")
+        }
+    }
+}
+
+#if os(Linux)
+extension ArgumentsTests {
+    static var allTests: [(String, (ArgumentsTests) -> () throws -> Void)] {
+        return [
+            ("testOpParse", testOpParse),
+            ("testArgParse", testArgParse),
+            ("testSwitchParse", testSwitchParse),
+            ("testInvalidArg", testInvalidArg),
+            ("testInvalidOp", testInvalidOp),
+            ("testInvalidSwitch", testInvalidSwitch)
+        ]
+    }
+}
+#endif
