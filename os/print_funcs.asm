ba0c5561dc26ee588e3ded04451e1d31abbe122b
fill1890 <andrew.walls2@gmail.com>
2016-06-24 15:13:43 +1000

    Implement print_hex correctly
    * [Added] print_hex now accurately prints given value

--- os/print_funcs.asm
+++ os/print_funcs.asm
@@ -1,9 +1,41 @@
+; Prints 4-digit hex code from dx
 print_hex:
-    ; TODO: Manipulate HEX_OUT to reflect dx
+    pusha           ; Save registers
+
+    ; dx: 0xd5f1
+    mov cx, 0       ; Counter
+    .loop:
+    mov al, dl      ; Take lowest two digits of number
+    
+    and al, 0x0F    ; Ignore upper digit
+    cmp al, 10      ; Test if letter or number
+    jge .high       
+
+    add al, 48
+    jmp .next
+
+    .high:
+    add al, 87
+    
+    .next:
+    
+    mov bx, HEX_OUT ; Initial position
+    add bx, 5       ; Trailing digit
+    sub bx, cx      ; Choose correct digit
+    mov [bx], al    ; Modify HEX_OUT
+
+    shr dx, 4       ; Select next digit
+    inc cx          ; Increment counter
+    cmp cx, 3       ; Terminate loop if complete
+    jle .loop
+
     mov bx, HEX_OUT
     call print_function
+
+    popa
     ret
 
+; Prints null-terminated string from bx
 print_function:
     pusha
     
